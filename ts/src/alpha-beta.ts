7b7f2659dc14584c6b6a27143f482878dfae787e
S-Cesc <cesc.sasal@gmail.com>
2022-08-29 06:46:03 +0200

    -

--- ts/src/alpha-beta.ts
+++ ts/src/alpha-beta.ts
@@ -8,7 +8,6 @@ import type {
 } from "./cescacs.types";
 
 
-
 import { csTypes as csty, csConvert as cscnv } from "./cescacs.types";
 import { PositionHelper } from "./cescacs.positionHelper";
 import { csPieceTypes as cspty } from "./cescacs.piece";
